f9313a0529512ee7f12dacc8d98c20c16c08ed0a
Le Viet <Zhu@Zhu.local>
2020-05-17 17:18:31 +0700

    doneee

--- routes/api/user/index.js
+++ routes/api/user/index.js
@@ -9,6 +9,8 @@ const router = express.Router();
 
 router.post("/login",userController.login);
 router.post("/",
+    authenticate,
+    authorize(["Admin"]),
     validateCreateUser,
     userController.createUser);
 router.get("/",
